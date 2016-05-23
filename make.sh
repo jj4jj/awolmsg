990dac61a11e8783cedd8cf2cba301302d8b37a3
jj4jj <resc@vip.qq.com>
2016-05-23 21:36:50 +0800

    commit

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 protoc=protoc
 protol="-lprotobuf"
+evl="-lev"
 if [ "x$1" = "xu1" ];then
     protoc="../../protobuf/bin/protoc"
     protol="../../protobuf/lib/libprotobuf.a"
     protoi="-I../../protobuf/include"
+    evl="../../libev/lib/libev.a"
 fi
 
 cd ./awolmsg && make install protoc="${protoc}" protol="${protol}" protoi="${protoi}"
@@ -13,4 +15,4 @@ cd ./app/ && make install protoc="${protoc}" protol="${protol}" protoi="${protoi
 cd -
 cd ./test/ && make install protoc="${protoc}" protol="${protol}" protoi="${protoi}"
 cd -
-cd ./msgsvr/ && make install protoc="${protoc}" protol="${protol}" protoi="${protoi}"
+cd ./msgsvr/ && make install protoc="${protoc}" protol="${protol}" protoi="${protoi}" evl="${evl}"
