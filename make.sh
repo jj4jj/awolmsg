4c743761445708f07c21267d4d9041de8636b369
jj4jj <resc@vip.qq.com>
2016-05-23 16:24:19 +0800

    change

--- make.sh
+++ make.sh
@@ -2,14 +2,15 @@
 protoc=protoc
 protol="-lprotobuf"
 if [ "x$1" = "xu1" ];then
-    protoc="../protobuf/bin/protoc"
-    protol="../protobuf/lib/libprotobuf.a"
+    protoc="../../protobuf/bin/protoc"
+    protol="../../protobuf/lib/libprotobuf.a"
+    protoi="-I../../protobuf/include"
 fi
 
-cd ./awolmsg && make install protoc="${protoc}" protol="${protol}"
+cd ./awolmsg && make install protoc="${protoc}" protol="${protol}" protoi="${protoi}"
 cd -
-cd ./app/ && make install protoc="${protoc}" protol="${protol}"
+cd ./app/ && make install protoc="${protoc}" protol="${protol}" protoi="${protoi}"
 cd -
-cd ./test/ && make install protoc="${protoc}" protol="${protol}"
+cd ./test/ && make install protoc="${protoc}" protol="${protol}" protoi="${protoi}"
 cd -
-cd ./msgsvr/ && make install protoc="${protoc}" protol="${protol}"
+cd ./msgsvr/ && make install protoc="${protoc}" protol="${protol}" protoi="${protoi}"
